4fb8a8882ba62819d9b5998a0e1bd73a0227f7d4
Lukas Lamberz <120056355+Lamboserker@users.noreply.github.com>
2024-04-17 19:22:15 +0200

    Update saleController.js

--- controller/saleController.js
+++ controller/saleController.js
@@ -3,22 +3,26 @@ import Sale from "../models/Sale.js";
 
 // Verkauf erfassen
 export const recordSale = async (req, res) => {
-  const { userId, productId, count, amount } = req.body; // Include `amount` here
+  if (!req.user) {
+    return res.status(401).json({ message: "Nicht autorisiert" });
+  }
+
+  const { userId, productId, count, amount } = req.body;
 
   try {
     const newSale = await Sale.create({
       user: userId,
       product: productId,
-      date: new Date(),
       count,
       amount,
+      date: new Date(),
     });
 
     res.status(201).json(newSale);
   } catch (error) {
     res.status(500).json({
       message: "Etwas ist schiefgelaufen beim Erfassen des Verkaufs.",
+      error: error.message
     });
-    console.log(error);
   }
 };
