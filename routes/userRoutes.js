acea091dd00eafd740104ba71f3554cb973f0375
Lamboserker <lukaslamberz96@gmail.com>
2024-04-24 17:08:26 +0200

    changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,8 @@
 import express from "express";
-import {
-  registerUser,
-  loginUser,
-  verifyEmail,
-} from "../controller/userController.js";
+import { registerUser, loginUser } from "../controller/userController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/verify-email", verifyEmail); 
 export default router;
